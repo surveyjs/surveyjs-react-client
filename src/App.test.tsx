67aa13b926614f8d0ebb46128fae5512def3c350
T_S_V <tsv2013@noreply.github.com>
2022-04-07 17:05:22 +0300

    Updated u-test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page header', () => {
   render(<App />);
   const linkElement = screen.getByText(/My Surveys/i);
   expect(linkElement).toBeInTheDocument();
