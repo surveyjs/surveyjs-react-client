8f9bb6564e3fd880800e60c713a3b395298d6471
T_S_V <tsv2013@noreply.github.com>
2022-04-07 17:18:50 +0300

    Added about text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
-const About = () => (<><h1>About</h1></>)
+const About = () => (<div className='sjs-client-app__content--about'>
+    <h1>About</h1>
+    <p>
+        This application demonatrates how to implement a very basic SurveyJS service client. It can be used with any backend similar described here - https://surveyjs.io/Help/Index?apiType=public
+    </p>
+    </div>)
 
 export default About;
