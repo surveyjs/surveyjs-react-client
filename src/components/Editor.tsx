8ce73768161b120fefa0b7015e3040d56a13496e
tsv2013 <tsv2013@noreply.github.com>
2024-02-21 12:34:15 +0300

    https://github.com/surveyjs/service/issues/1972 - bump SurveyJS product versions

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -9,6 +9,7 @@ const Editor = (params: { id: string }): React.ReactElement => {
     const creator = useMemo(() => {
         const options = {
             showLogicTab: true,
+            showThemeTab: true,
             showTranslationTab: true
         };
         return new SurveyCreator(options);
