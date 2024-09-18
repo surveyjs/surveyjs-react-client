df123a9b1409bb3e7fe7483311561657a897a3fd
carlospuco <carlos.puco.mb@gmail.com>
2024-09-18 01:14:14 -0500

    Update dependencys typescript, web-vitals and msw. Update handlers and reportWebVitals

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,9 @@
-import { rest } from 'msw'
+import { http, HttpResponse } from 'msw'
 import { createSurvey, getResults, getSurvey, getSurveys, postResult, removeSurvey, updateSurvey } from '../models/in-memory-storage'
 import { apiBaseAddress } from '../models/survey'
 
 export const handlers = [
-    rest.get(apiBaseAddress + '/surveys', (req, res, ctx) => {
+    http.get(apiBaseAddress + '/surveys', ({ request, params, cookies }) => {
         // const { userId } = req.params
         // return res(
         //   ctx.json({
@@ -12,51 +12,36 @@ export const handlers = [
         //     lastName: 'Maverick',
         //   }),
         // )
-        return res(
-            ctx.json(getSurveys()),
-        )
+        return HttpResponse.json(getSurveys())
     }),
-    rest.get(apiBaseAddress + '/getActive', (req, res, ctx) => {
-        return res(
-            ctx.json(getSurveys()),
-        )
+    http.get(apiBaseAddress + '/getActive', ({ request, params, cookies }) => {
+        return HttpResponse.json(getSurveys())
     }),
-    rest.get(apiBaseAddress + '/create', (req, res, ctx) => {
-        return res(
-            ctx.json(createSurvey()),
-        )
+    http.get(apiBaseAddress + '/create', ({ request, params, cookies }) => {
+        return HttpResponse.json(createSurvey())
     }),
-    rest.get(apiBaseAddress + '/delete', (req, res, ctx) => {
-        const id = req.url.searchParams.get('id')
-        removeSurvey(id as string);
-        return res(
-            ctx.json({ id }),
-        )
+    http.get(apiBaseAddress + '/delete', ({ request, params, cookies }) => {
+        const id = request.url.search('id');
+        // const id = req.url.searchParams.get('id')
+        removeSurvey(id.toString());
+        return HttpResponse.json({ id })
     }),
-    rest.get(apiBaseAddress + '/getSurvey', (req, res, ctx) => {
-        const surveyId = req.url.searchParams.get('surveyId')
-        return res(
-            ctx.json(getSurvey(surveyId as string)),
-        )
+    http.get(apiBaseAddress + '/getSurvey', ({ request, params, cookies }) => {
+        const surveyId = request.url.search('surveyId');
+        return HttpResponse.json(getSurvey(surveyId.toString()))
     }),
-    rest.post(apiBaseAddress + '/changeJson', (req, res, ctx) => {
-        const { id, json } = req.body as Record<string, any>
+    http.post(apiBaseAddress + '/changeJson', ({ request, params, cookies }) => {
+        const { id, json } = request.body as Record<string, any>
         updateSurvey(id as string, json)
-        return res(
-            ctx.json({ id, json }),
-        )
+        return HttpResponse.json({ id, json })
     }),
-    rest.post(apiBaseAddress + '/post', (req, res, ctx) => {
-        const { postId, surveyResult } = req.body as Record<string, any>
+    http.post(apiBaseAddress + '/post', ({ request, params, cookies }) => {
+        const { postId, surveyResult } = request.body as Record<string, any>
         postResult(postId as string, surveyResult)
-        return res(
-            ctx.json({}),
-        )
+        return HttpResponse.json({})
     }),
-    rest.get(apiBaseAddress + '/results', (req, res, ctx) => {
-        const postId = req.url.searchParams.get('postId')
-        return res(
-            ctx.json({ id: postId, data: getResults(postId as string) }),
-        )
+    http.get(apiBaseAddress + '/results', ({ request, params, cookies }) => {
+        const postId = request.url.search('postId');
+        return HttpResponse.json({ id: postId, data: getResults(postId.toString()) })
     })
 ]
